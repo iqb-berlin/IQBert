68c67ded95014e1789bb4106f8127cc537142770
paflov <philipp.franck@hu-berlin.de>
2025-06-25 11:17:21 +0200

    VIII

--- experiments/slurm/predict2.py
+++ experiments/slurm/predict2.py
@@ -11,13 +11,13 @@ data = get_data(sys.argv[1])
 result = []
 
 for datapoint in data:
-    inputs = tokenizer(datapoint.value, return_tensors="pt")
+    inputs = tokenizer(datapoint['value'], return_tensors="pt")
     with torch.no_grad():
         logits = model(**inputs).logits
         predicted_class = torch.argmax(logits).item()
 
-    print("Input: ", datapoint.value)
+    print("Input: ", datapoint['value'])
     print("Predicted class: ", predicted_class)
-    result.append({"value": datapoint.value, "class": predicted_class})
+    result.append({"value": datapoint['value'], "class": predicted_class})
 
 store_data(result, sys.argv[2])
